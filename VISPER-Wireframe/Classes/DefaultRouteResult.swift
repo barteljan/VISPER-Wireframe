5fe7410bfc2dea8e0a634b468d27e8dd5ab2aff3
Jan Bartel <jan.bartel@atino.net>
2017-12-01 08:27:04 +0100

    added isEqual to RouteResult Protocol

--- VISPER-Wireframe/Classes/DefaultRouteResult.swift
+++ VISPER-Wireframe/Classes/DefaultRouteResult.swift
@@ -9,7 +9,7 @@ import Foundation
 import VISPER_Wireframe_Core
 
 public struct DefaultRouteResult : RouteResult, Equatable {
-
+    
     public let routePattern: String
     public let parameters: [String : Any]
     public var routingOption: RoutingOption?
@@ -25,21 +25,7 @@ public struct DefaultRouteResult : RouteResult, Equatable {
     }
     
     public static func ==(lhs: DefaultRouteResult, rhs: DefaultRouteResult) -> Bool {
-        
-        let lhsParams = NSDictionary(dictionary: lhs.parameters)
-        let rhsParams = NSDictionary(dictionary: rhs.parameters)
-        
-        var optionsAreEqual = false
-        
-        if lhs.routingOption == nil && rhs.routingOption == nil {
-            return true
-        } else if let routingOption = lhs.routingOption {
-            optionsAreEqual = routingOption.isEqual(otherOption:rhs.routingOption)
-        }
-        
-        return lhs.routePattern == rhs.routePattern &&
-               lhsParams == rhsParams &&
-               optionsAreEqual
+        return lhs.isEqual(routeResult: rhs)
     }
     
 }
